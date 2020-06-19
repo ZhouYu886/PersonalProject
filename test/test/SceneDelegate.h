96df45c19f488755d299f7ab91b0ea7b8f0b7a78
ZhouYu <447754352@qq.com>
2020-06-19 09:26:53 +0800

    test

---
+++ test/test/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  test
+//
+//  Created by apple on 2020/6/19.
+//  Copyright © 2020 123123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by apple on 2020/6/19.
+//  Copyright © 2020 123123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
